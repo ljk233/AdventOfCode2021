5f0c4ff24ff639345158054a09e0b4aaba29e80f
Liam Kelly <liam.james.kelly@gmail.com>
2021-12-25 12:36:20 +0000

    day 5 initialise

---
+++ days/05/d5.jl
@@ -0,0 +1,97 @@
+### A Pluto.jl notebook ###
+# v0.17.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 87c8c810-657e-11ec-2114-4b4ad4af6ec8
+md"""
+# Day 5: Hydrothermal Venture
+
+**Reference:** [Day 5 - Advent of Code 2021](https://adventofcode.com/2021/day/5)
+
+**Source:** []()
+"""
+
+# ╔═╡ b86823bf-1386-449f-8940-17aa45a0d242
+md"""
+## Imports
+"""
+
+# ╔═╡ 462a095b-9508-44c3-9507-6e391ec990b9
+
+
+# ╔═╡ 3fa9442f-7395-45f5-97b1-e28dcd065514
+
+
+# ╔═╡ e05f10b5-f57e-42ca-8ee6-5df06c0baa59
+md"""
+## Functions
+"""
+
+# ╔═╡ 5ae9f2df-e262-47d7-8fc9-19272f6883ef
+
+
+# ╔═╡ f0e7076e-56fa-4608-bd82-75d99289c206
+
+
+# ╔═╡ 4c202df3-62b5-4446-b3e1-0afae48210b8
+md"""
+## Part One: Dodge The Danger
+
+To avoid the most dangerous areas, you need to determine the number of points where at least two lines overlap.
+Consider only horizontal and vertical lines.
+
+**At how many points do at least two lines overlap?**
+"""
+
+# ╔═╡ 1a9694c2-923b-4f98-83f6-2b2bf3ff55ab
+"""
+	main1(f: String) -> Integer
+"""
+function main1(f::String)
+end
+
+# ╔═╡ c2a8fd18-c49e-48e0-8780-0591a3c59fa6
+
+
+# ╔═╡ a5ac2615-cae0-4a16-901d-687fd46f3145
+
+
+# ╔═╡ a957d84e-b1a4-4a95-8539-fcaf07f1f1e5
+
+
+# ╔═╡ d6b93da4-1ef1-48d6-83fe-8424acbcfaf2
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─87c8c810-657e-11ec-2114-4b4ad4af6ec8
+# ╟─b86823bf-1386-449f-8940-17aa45a0d242
+# ╠═462a095b-9508-44c3-9507-6e391ec990b9
+# ╠═3fa9442f-7395-45f5-97b1-e28dcd065514
+# ╟─e05f10b5-f57e-42ca-8ee6-5df06c0baa59
+# ╠═5ae9f2df-e262-47d7-8fc9-19272f6883ef
+# ╠═f0e7076e-56fa-4608-bd82-75d99289c206
+# ╟─4c202df3-62b5-4446-b3e1-0afae48210b8
+# ╠═1a9694c2-923b-4f98-83f6-2b2bf3ff55ab
+# ╠═c2a8fd18-c49e-48e0-8780-0591a3c59fa6
+# ╠═a5ac2615-cae0-4a16-901d-687fd46f3145
+# ╠═a957d84e-b1a4-4a95-8539-fcaf07f1f1e5
+# ╠═d6b93da4-1ef1-48d6-83fe-8424acbcfaf2
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
